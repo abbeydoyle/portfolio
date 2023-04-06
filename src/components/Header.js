ba0dd9ec81665fa260e23245ece144b6b3086f2a
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-06 11:08:48 -0700

    header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import logo from "../assets/logo.png"
 // navbar header
 function Header() {
   return (
-    <Navbar fluid={true} rounded={true}>
+    <Navbar fluid={true} rounded={true} className="bg-[url('https://res.cloudinary.com/duxmtidm1/image/upload/v1680803471/389877_olc7hw.jpg')] w-full header">
         <Navbar.Brand
           href="/"
         >
@@ -15,46 +15,46 @@ function Header() {
             className="mr-3 h-6 sm:h-9"
             alt="Logo"
           />
-          <span className="self-center md:text-2xl font-semibold whitespace-nowrap text-[#3F5451]">
+          <span className="self-center md:text-2xl font-semibold whitespace-nowrap text-white">
             Abigail Doyle
           </span>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Navbar.Link href="/">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 hover:text-[#3F5451] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
               About
             </span>
           </Navbar.Link>
           <Navbar.Link href="/portfolio">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 hover:text-[#3F5451] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
               Portfolio
             </span>
           </Navbar.Link>
           <Navbar.Link href="/career">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 hover:text-[#3F5451] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
               Career
             </span>
           </Navbar.Link>
           <Navbar.Link href="/volunteer">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 hover:text-[#3F5451] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
               Volunteer
             </span>
           </Navbar.Link>
           <Navbar.Link href="/publications">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 hover:text-[#3F5451] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
               Publications
             </span>
           </Navbar.Link>
           <Navbar.Link onClick={() => {
               
             }}>
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent text-white md:p-0">
               Resume
             </span>
           </Navbar.Link>
           <Navbar.Link href="/contact">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 hover:text-[#3F5451] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:text-[#c4d4e0] hover:underline">
               Contact
             </span>
           </Navbar.Link>
