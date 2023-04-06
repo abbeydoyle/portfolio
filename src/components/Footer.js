d9c0ac55d38b54cc3bb725eca4e201e5998dd99d
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-05 20:37:50 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 // dependencies
 import React from "react";
+import { Footer } from "flowbite-react";
 
 // export linkable footer
 export default function Footer() {
   return (
-    <></>
+    <Footer container={true} className="flex justify-center">
+      <Footer.Copyright
+        href="https://github.com/abbeydoyle/portfolio/blob/main/LICENSE"
+        by="Abigail Doyle"
+        year={2023}
+      />
+    </Footer>
   );
 }
