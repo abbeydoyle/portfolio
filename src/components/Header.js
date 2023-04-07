24924a04a3eeda1c92b1b82aae01b4af225f9548
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-06 19:24:22 -0700

    about homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import logo from "../assets/logo.png"
 // navbar header
 function Header() {
   return (
-    <Navbar fluid={true} rounded={true} className="bg-[url('https://res.cloudinary.com/duxmtidm1/image/upload/v1680803471/389877_olc7hw.jpg')] w-full header">
+    <Navbar fluid={true} className="md:bg-[url('https://res.cloudinary.com/duxmtidm1/image/upload/v1680803471/389877_olc7hw.jpg')] header">
         <Navbar.Brand
           href="/"
         >
@@ -15,46 +15,46 @@ function Header() {
             className="mr-3 h-6 sm:h-9"
             alt="Logo"
           />
-          <span className="self-center md:text-2xl font-semibold whitespace-nowrap text-white">
+          <span className="self-center md:text-2xl font-semibold whitespace-nowrap md:text-white text-[#3F5451]">
             Abigail Doyle
           </span>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Navbar.Link href="/">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 md:text-white hover:underline text-[#3F5451]">
               About
             </span>
           </Navbar.Link>
           <Navbar.Link href="/portfolio">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 md:text-white hover:underline text-[#3F5451]">
               Portfolio
             </span>
           </Navbar.Link>
           <Navbar.Link href="/career">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 md:text-white hover:underline text-[#3F5451]">
               Career
             </span>
           </Navbar.Link>
           <Navbar.Link href="/volunteer">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 md:text-white hover:underline text-[#3F5451]">
               Volunteer
             </span>
           </Navbar.Link>
           <Navbar.Link href="/publications">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 md:text-white hover:underline text-[#3F5451]">
               Publications
             </span>
           </Navbar.Link>
           <Navbar.Link onClick={() => {
               
             }}>
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent text-white md:p-0">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:text-white md:p-0 text-[#3F5451]">
               Resume
             </span>
           </Navbar.Link>
           <Navbar.Link href="/contact">
-            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 text-white hover:text-[#c4d4e0] hover:underline">
+            <span className="block md:py-2 pl-3 pr-4 md:text-lg rounded bg-transparent md:p-0 md:text-white text-[#3F5451] hover:underline">
               Contact
             </span>
           </Navbar.Link>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,77 @@
 import React from "react";
 
+const svgStyle = {
+  height: "560px",
+  width: "100%",
+  zIndex: "-10",
+  overflow: "hidden",
+  marginTop: "-40vw",
+};
+
 function About() {
   return (
-  <>About</>
+    <>
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        preserveAspectRatio="none"
+        className="svg absolute hidden lg:block"
+        style={svgStyle}
+      >
+        <defs>
+          <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+            <stop stop-color="rgb(242,235,230)" offset="0%"></stop>
+            <stop stop-color="rgb(210,183,164)" offset="100%"></stop>
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#sw-gradient-0)"
+          d="M 0.351 264.418 C 0.351 264.418 33.396 268.165 47.112 270.128 C 265.033 301.319 477.487 325.608 614.827 237.124 C 713.575 173.504 692.613 144.116 805.776 87.876 C 942.649 19.853 1317.845 20.149 1440.003 23.965 C 1466.069 24.779 1440.135 24.024 1440.135 24.024 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 L 0.351 264.418 Z"
+        ></path>
+      </svg>
+
+      <h1 className="pt-5 text-[#3F5451] font-bold">About Me</h1>
+
+      <div className="px-6 py-12 lg:my-12 md:px-12 text-gray-800 text-center lg:text-left">
+        <div className="container mx-auto xl:px-32">
+          <div className="grid lg:grid-cols-2 gap-12 flex">
+            <div className="mt-12 lg:mt-0">
+              <p className="text-lg self-start md:leading-10 ">
+                My name is Abbey Doyle. I'm an environmental engineer working
+                for Boeing, currently in the progress of getting a Professional
+                Certificate in full stack development at the University of
+                Washington. After growing up in Kingsport, TN I left to get my
+                bachelor's degree in Chattanooga, TN. There I studied chemical
+                engineering and biology while in the Brock Scholars Program. I
+                completed an undergraduate thesis while researching the
+                pathogenicity of Vibrio cholerae and completed internships and
+                the Center for Disease Control and Prevention and Chattem
+                Sanofi. After graduating I began my career as a research chemist
+                at the CDC through Battelle and worked in the Lung Injury and
+                COVID-19 responses. After living in Atlanta for 3 years I moved
+                crosscountry to Seattle, where I reside in my current job role.
+                In my free time I like to backpack, read, run, play the violin
+                or piano, and take my dog to parks. Throughout my education and
+                career I learned languages like: Python, MATLAB, VBA, CAD
+                software, JS, CSS, HTML, RestFUL APIs, the MERN stack,
+                object-oriented programing, etc. I became fascinated with all of
+                the minor differences in softwares that make or break a code and
+                love the challenges that arise when writing a method start to
+                finish.
+              </p>
+            </div>
+            <div className="mb-12 lg:mb-0">
+              <img
+                src="https://res.cloudinary.com/duxmtidm1/image/upload/v1680833230/IMG_1308_il2unr.png"
+                className="w-full rounded-lg shadow-lg"
+                alt="profile image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
