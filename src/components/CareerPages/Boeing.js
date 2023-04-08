565d9637ff02d1f4b647093ed0f3d54b08b17ada
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-07 20:09:56 -0700

    boeing blurb

--- src/components/CareerPages/Boeing.js
+++ src/components/CareerPages/Boeing.js
@@ -12,13 +12,10 @@ export default function Boeing() {
             <div className="object-cover rounded-lg bg-[#b48868] p-5 pl-8">
 
               <p className="pr-[30%]">
-                + Create queries within Access databases to profile hazardous
-                waste designationsto ensure safe practices and
-                regulatory compliance
+              Boeing is one of the world's largest aerospace companies, known for designing, manufacturing, and selling airplanes, helicopters, satellites, and missiles. Founded in Seattle, Washington in 1916, the company has grown to become a global leader in the aviation industry, with operations in over 65 countries and a workforce of more than 140,000 employees. Boeing is known for its iconic commercial jetliners such as the 747, 777, and 787, as well as its defense and space products. The company's commitment to innovation and safety has made it a trusted brand in the aviation industry for over a century.
               </p>
               <p  className="pr-[30%]">
-                + Routinely create required permit reports to the appropriate
-                agency forenvironmental, health, and safety standards
+              As an environmental engineer at Boeing, I work to ensure that the company operates in an environmentally sustainable and responsible manner. This involves developing and implementing strategies to minimize the environmental impact of the company's operations, such as reducing greenhouse gas emissions, managing hazardous waste, and conserving natural resources. I work daily with other engineers, scientists, and environmental professionals to identify and mitigate environmental risks and to develop innovative solutions to complex environmental challenges. My favorite tasks typically revolve around remediation and conservation, working Boeing's commitment to sustainability and responsible environmental stewardship, while helping to ensure the company's continued success in the aviation industry.
               </p>
 
               <button
