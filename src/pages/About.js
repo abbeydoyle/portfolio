58580307ec0e314206007237a8402af90c78e989
Abigail Doyle <abigaildoyle3@gmail.com>
2023-04-13 09:20:43 -0700

    git rid of spring

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Backpacking from "../components/Hobbies/Backpacking";
 import Duna from "../components/Hobbies/Duna";
 import Reading from "../components/Hobbies/Reading";
